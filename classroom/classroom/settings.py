2189903d73d265cee52bd3a7cb251f59aaa6817d
nznnnks <evilrobbitop2006@gmail.com>
2025-09-20 00:13:13 +0300

    Pagination setting

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -148,3 +148,8 @@ MEDIA_ROOT = BASE_DIR / MEDIA_URL
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
