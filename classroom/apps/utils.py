9cbe9dc29e18000a06d3f17e1063f8805c4ad0ac
nznnnks <evilrobbitop2006@gmail.com>
2025-09-21 16:49:05 +0300

    Codes generator

--- classroom/apps/utils.py
+++ classroom/apps/utils.py
@@ -1,12 +1,32 @@
 import os
 import secrets
 import string
+import random
+from datetime import timedelta
+from django.utils import timezone
 
+def generate_random_string(length, use_upper_case=True, use_digits=True):
+    characters = ''
+    if use_upper_case:
+        characters += string.ascii_letters
+    else:
+        characters += string.ascii_lowercase
+    if use_digits:
+        characters += string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
 
-def generate_random_string(length, use_upper_case=True):
-    characters = string.ascii_letters if use_upper_case else string.ascii_lowercase
-    characters += string.digits
-    return ''.join(secrets.choice(characters) for _ in range(length))
+def generate_verification_code(length=6):
+    return generate_random_string(
+        length=length,
+        use_upper_case=False,
+        use_digits=True
+    )
+
+def is_verification_code_expired(sent_at):
+    if not sent_at:
+        return True
+    expiration_time = sent_at + timedelta(minutes=10)
+    return timezone.now() > expiration_time
 
 
 def file_upload_path(prefix, instance, filename, directory=None):
