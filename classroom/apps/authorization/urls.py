fa42bb55b4b2f3fa06c2c1a730a3e2ffb8200891
nznnnks <evilrobbitop2006@gmail.com>
2025-09-23 12:42:01 +0300

    Verification code url

--- classroom/apps/authorization/urls.py
+++ classroom/apps/authorization/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import LoginView, logout_view, verify_email
-
+from .views import LoginView, logout_view, verify_email, request_verification_code
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', logout_view, name="logout"),
     path('verify-email/', verify_email, name="verify-email"),
+    path('request-verification-code/', request_verification_code, name="request-verification-code"),
 ]
