01e7de5942f0070b93e9dd2e17665e633cbe6907
nznnnks <evilrobbitop2006@gmail.com>
2025-09-23 12:40:29 +0300

    Auth func was deleted

--- classroom/apps/authorization/permissions.py
+++ classroom/apps/authorization/permissions.py
@@ -1,19 +1,8 @@
 from rest_framework import permissions
 from django.utils.translation import gettext_lazy as _
-from .services import send_verification_email
 
 class IsVerified(permissions.BasePermission):
     message = _("Пользователь не верифицирован. Доступ запрещен.")
     
     def has_permission(self, request, view):
-        if not request.user or not request.user.is_authenticated:
-            return False
-            
-        if request.user.is_verified:
-            return True
-            
-        if not request.user.is_verified:
-            send_verification_email(request.user)
-            
-        return False
-        
+        return request.user.is_verified
