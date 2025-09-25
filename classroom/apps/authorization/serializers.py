4c01d448bc6ba36f388c10cd86af34e7617e4b41
nznnnks <evilrobbitop2006@gmail.com>
2025-09-25 22:24:57 +0300

    New func to change password with mail

--- classroom/apps/authorization/serializers.py
+++ classroom/apps/authorization/serializers.py
@@ -5,6 +5,9 @@ from rest_framework.exceptions import ValidationError
 
 from .models import CustomUser
 
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
 
 class UserProfileSerializer(ModelSerializer):
 
@@ -54,3 +57,60 @@ class LoginUserSerializer(Serializer):
             )
         attrs['user'] = user
         return attrs
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Старый пароль неверен")
+        return value
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"confirm_password": "Пароли не совпадают"})
+        
+        validate_password(attrs['new_password'])
+        return attrs
+
+    def save(self, **kwargs):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
+
+
+class RequestPasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+    def validate_email(self, value):
+        try:
+            user = CustomUser.objects.get(email=value)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("Пользователь с таким email не найден")
+        return value
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    code = serializers.CharField(max_length=6, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"confirm_password": "Пароли не совпадают"})
+        
+        validate_password(attrs['new_password'])
+        return attrs
+
+    def validate_email(self, value):
+        try:
+            user = CustomUser.objects.get(email=value)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("Пользователь с таким email не найден")
+        return value
