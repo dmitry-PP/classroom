85363c0c527961cfd07b832b91237a771ad0d3e0
nznnnks <evilrobbitop2006@gmail.com>
2025-09-25 22:25:34 +0300

    New func to change password with mail

--- classroom/apps/authorization/services.py
+++ classroom/apps/authorization/services.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from apps.utils import generate_random_string
 from django.utils import timezone
-from .models import VerifiedCodesModel
+from .models import VerifiedCodesModel, PasswordResetCodesModel
 
 def send_verification_email(user):
     code = generate_random_string(
@@ -36,3 +36,38 @@ def send_verification_email(user):
     )
     
     return code
+
+def send_password_reset_email(user):
+    """Отправка кода для сброса пароля"""
+    reset_code = generate_random_string(
+        length=6, 
+        use_upper_case=False, 
+        use_digits=True
+    )
+
+    
+    PasswordResetCodesModel.objects.filter(user=user).delete()
+    
+    reset_code_obj = PasswordResetCodesModel.objects.create(
+        user=user,
+        code=reset_code
+    )
+    
+    subject = 'Код для сброса пароля'
+    message = f'''
+    Ваш код для сброса пароля: {reset_code}
+    
+    Код действителен в течение 10 минут.
+    
+    Если вы не запрашивали сброс пароля, проигнорируйте это письмо.
+    '''
+    
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+        fail_silently=False,
+    )
+    
+    return reset_code
